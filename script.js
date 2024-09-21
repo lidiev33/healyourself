35c299fa133b63be7bcec2806fcac72e96f388c5
lidiev33 <vidalconceptdesigns@gmail.com>
2024-09-21 02:48:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -67,21 +67,19 @@ document.addEventListener('DOMContentLoaded', function () {
   displayDailyReports();
   displayWeeklyAverage();
 
-  // Your Firebase configuration from step 1
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyDAcuQTkeAHOMbazn-Dmf9KuCPFuI3sdyk",
+  authDomain: "healing-site.firebaseapp.com",
+  projectId: "healing-site",
+  storageBucket: "healing-site.appspot.com",
+  messagingSenderId: "414554343739",
+  appId: "1:414554343739:web:75083b9eedf3f59d738f1e",
+  measurementId: "G-3TV74H6QE7"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Initialize Firebase Database
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 });
 
